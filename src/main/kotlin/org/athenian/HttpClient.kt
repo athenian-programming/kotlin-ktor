00c650b85854125ddb00e9ee7cf2dd79fd9b861a
pambrose <pambrose@mac.com>
2019-11-27 22:58:42 -0800

    Merge branch 'master' of /Users/pambrose/git/kotlin-ktor with conflicts.

---
+++ src/main/kotlin/org/athenian/HttpClient.kt
@@ -0,0 +1,21 @@
+package org.athenian
+
+import com.github.pambrose.common.dsl.KtorDsl
+import com.github.pambrose.common.dsl.KtorDsl.get
+import com.github.pambrose.common.dsl.KtorDsl.http
+import com.github.pambrose.common.dsl.KtorDsl.newHttpClient
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+
+  val url = "http://localhost:8080/json-ez"
+  //val client = newHttpClient()
+
+  runBlocking {
+    //http {
+      //get(url, ))
+    //}
+  }
+
+
+}
